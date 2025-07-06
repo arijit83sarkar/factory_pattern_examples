4ce489da3764204677046dfedba3ea60e7cc09d6
arijit83sarkar <mail2arijit83@gmail.com>
2025-07-06 21:08:44 +0530

    The Abstract Hero Agency (The Creator) #5

--- src/com/raven/example_superhero/with_factory/defination/ISuperhero.java
+++ src/com/raven/example_superhero/with_factory/defination/ISuperhero.java
@@ -1,4 +1,4 @@
-package com.raven.example_superhero.with_factory;
+package com.raven.example_superhero.with_factory.defination;
 
 // The sacred oath of all heroes! This is our "Product" interface.
 public interface ISuperhero {
